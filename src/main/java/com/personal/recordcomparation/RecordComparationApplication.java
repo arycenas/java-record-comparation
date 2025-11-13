abc501431b8b8a797f9cb527c9d154a7cdb4cc6a
Steven Arycena Fatich <steven.a.fatich@gli.id>
2025-11-13 11:15:35 +0700

    Initial commit

---
+++ src/main/java/com/personal/recordcomparation/RecordComparationApplication.java
@@ -0,0 +1,12 @@
+package com.personal.recordcomparation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordComparationApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(RecordComparationApplication.class, args);
+    }
+}
---
+++ src/test/java/com/personal/recordcomparation/RecordComparationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.recordcomparation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordComparationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
